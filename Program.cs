166b8401a58bfd7adc340619e1546072330cf11d
pdrlcx <mendesv460@gmail.com>
2022-07-19 20:39:29 -0300

    Adicionando Exercicio 6

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Classe_R6.src;
+using System;
+
+namespace Classe_R6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Agentes Kapkan = new Agentes("Kapkan", "Rússia", "Dispositivo Bloqueador", 2, "Defesa");
+            Agentes Sledge = new Agentes("Sledge", "Escócia", "Marreta de Demolição", 2, "Ataque");
+            Agentes Thermite = new Agentes("Thermite", "Estados Unidos","Carga Explosiva", 3, "Ataque");
+            Agentes Frost = new Agentes("Frost", "Canadá", "Capacho Armadilha", 3, "Defesa");
+
+
+            Console.WriteLine(Kapkan.ToString());
+            Console.WriteLine(Sledge.ToString());
+            Console.WriteLine(Frost.ToString());
+            Console.WriteLine(Thermite.ToString());  
+            
+        }
+    }
+}
---
+++ src/Agentes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_R6.src
+{
+    public class Agentes
+    {
+        string Nome { get; set; }
+        string País { get; set; }
+        string Habilidade { get; set; }
+        int Velocidade { get; set; }
+        string Função { get; set; }
+
+        public Agentes()
+        {
+
+        }
+
+        public Agentes(string nome, string país, string habilidade, int velocidade, string função)
+        {
+            Nome= nome;
+            País = país;
+            Habilidade = habilidade;
+            Velocidade = velocidade;
+            Função = função;
+        }
+
+        public override string ToString()
+        {
+            return $" Nome: {Nome}\n " +
+                   $" País: {País}\n " +
+                   $" Habilidade: {Habilidade}\n " +
+                   $" Velocidade: {Velocidade}\n " +
+                   $" Função: {Função}\n " ;
+        }
+
+    }
+
+}
+
